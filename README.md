13369fecc7baf6ca70c66692e1b12c73cf1f04e7
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-04-15 17:45:19 +0300

    feat: add interactions with example1 smart contract

--- README.md
+++ README.md
@@ -9,3 +9,10 @@
 - [@solana/wallet-adapter](https://github.com/solana-labs/wallet-adapter)
 - [@solana/wallet-adapter-example](https://github.com/solana-labs/wallet-adapter/tree/master/packages/starter/example)
 - [The Complete Guide to Full Stack Solana Development with React, Anchor, Rust, and Phantom](https://dev.to/edge-and-node/the-complete-guide-to-full-stack-solana-development-with-react-anchor-rust-and-phantom-3291)
+
+## Getting the project up and running
+
+## TODOs
+- Add scripts for `Getting the project up and running`.
+- Currently some type related errors are suppressed by `@ts-expect-error`. This comment should be removed by typing them correctly.
+- Consider [You'll notice that when you refresh, you lose the state of the program. This is because we are dynamically generating the base account when the program loads. If you wanted to read and interact with the program data across various clients, you would need to create and store the Keypair somewhere in your project. I've put together a gist of a naive approach of how this might look](https://gist.github.com/dabit3/7cbd18b8bc4b495c4831f8674902eb42).
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,133 @@
 
+import * as React from 'react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useWallet } from '@solana/wallet-adapter-react';
 import {
-  WalletDisconnectButton,
-  WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
+  Connection,
+  PublicKey
+} from '@solana/web3.js';
+import {
+  Program,
+  AnchorProvider,
+  web3
+} from '@project-serum/anchor';
 
-import SendOneLamportToRandomAddress from 'containers/SendOneLamportToRandomAddress';
-import AutoConnectSwitch from 'containers/AutoConnectSwitch';
-import RequestAirdrop from 'containers/RequestAirdrop';
-import SignMessage from 'containers/SignMessage';
+import { LOCAL_NETWORK_CONNECTION_ENDPOINT } from 'config/network';
+import EXAMPLE1_IDL from 'idls/example1.json';
+
+const {
+  SystemProgram,
+  Keypair
+} = web3;
+
+/* Create an account  */
+const baseAccount = Keypair.generate();
+const opts = {
+  preflightCommitment: 'processed'
+};
+const programID = new PublicKey(EXAMPLE1_IDL.metadata.address);
 
 const App = () => {
-  return (
-    <>
-      <SignMessage />
-      <AutoConnectSwitch />
-      <RequestAirdrop />
-      <WalletMultiButton />
-      <WalletDisconnectButton />
-      <SendOneLamportToRandomAddress />
-    </>
-  );
+  const [value, setValue] = React.useState(null);
+  const wallet = useWallet();
+
+  async function getProvider() {
+    /* Create the provider and return it to the caller */
+    /* Network set to local network for now */
+    const network = LOCAL_NETWORK_CONNECTION_ENDPOINT;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const connection = new Connection(network, opts.preflightCommitment);
+
+    const provider = new AnchorProvider(
+      connection,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      wallet,
+      opts.preflightCommitment
+    );
+
+    return provider;
+  }
+
+  async function createCounter() {    
+    const provider = await getProvider();
+    /* Create the program interface combining the idl, program ID, and provider */
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const program = new Program(EXAMPLE1_IDL, programID, provider);
+    try {
+      /* Interact with the program via rpc */
+      await program.rpc.create({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId
+        },
+        signers: [baseAccount]
+      });
+
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      console.log('account: ', account);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      setValue(account.count.toString());
+    } catch (error) {
+      console.log('Transaction error: ', error);
+    }
+  }
+
+  async function increment() {
+    const provider = await getProvider();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const program = new Program(EXAMPLE1_IDL, programID, provider);
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey
+      }
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('account: ', account);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    setValue(account.count.toString());
+  }
+
+  if (!wallet.connected) {
+    /* If the user's wallet is not connected, display connect wallet button */
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop:'100px'
+        }}>
+        <WalletMultiButton />
+      </div>
+    )
+  } else {
+    return (
+      <div className='App'>
+        {!value && (
+          <button onClick={createCounter}>
+            Create counter
+          </button>
+        )}
+        {value && (
+          <button onClick={increment}>
+            Increment counter
+          </button>
+        )}
+        {value && value >= Number(0) ? (
+          <h2>{value}</h2>
+        ) : (
+          <h3>Please create the counter.</h3>
+        )}
+      </div>
+    );
+  }
 };
 
 export default App;
