f1513b8dba0f416491107c93ff4b5d74473324b6
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-04-17 12:50:04 +0300

    chore: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 
+import {
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+
+import SendOneLamportToRandomAddress from 'containers/SendOneLamportToRandomAddress';
+import AutoConnectSwitch from 'containers/AutoConnectSwitch';
+import RequestAirdrop from 'containers/RequestAirdrop';
+import SignMessage from 'containers/SignMessage';
 import Example1 from 'containers/Example1';
 
 const App = () => {
   return (
-    <Example1 />
+    <>
+      <SignMessage />
+      <AutoConnectSwitch />
+      <RequestAirdrop />
+      <WalletMultiButton />
+      <WalletDisconnectButton />
+      <SendOneLamportToRandomAddress />
+      <Example1 />
+    </>
   );
 };
 
